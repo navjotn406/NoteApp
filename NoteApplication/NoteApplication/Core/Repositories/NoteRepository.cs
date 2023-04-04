fba5651fe1a9516128c8610843d51c6ae87775f3
Navjot Singh <navjotn406@gmail.com>
2023-04-04 19:51:31 +0600

    Create repositories.

---
+++ NoteApplication/NoteApplication/Core/Repositories/NoteRepository.cs
@@ -0,0 +1,12 @@
+﻿using NoteApplication.Core.IRepositories;
+using NoteApplication.Models;
+
+namespace NoteApplication.Core.Repositories
+{
+    public class NoteRepository : GenericRepository<Note>, INoteRepository
+    {
+        public NoteRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
