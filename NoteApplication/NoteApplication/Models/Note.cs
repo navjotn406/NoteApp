855966a75a6dcb3dad633c038aaff8fe5c5616d8
Navjot Singh <navjotn406@gmail.com>
2023-04-04 19:45:22 +0600

    Create User and Note models

---
+++ NoteApplication/NoteApplication/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace NoteApplication.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string UserId { get; set; }
+        public string Category { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public User User { get; set; }
+    }
+}
