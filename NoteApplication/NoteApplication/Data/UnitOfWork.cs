1edb7983b7becaa831c8caabb60b140166cfb58d
Navjot Singh <navjotn406@gmail.com>
2023-04-04 19:54:46 +0600

    Create application db context

---
+++ NoteApplication/NoteApplication/Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using NoteApplication.Core.Configuration;
+using NoteApplication.Core.IRepositories;
+using NoteApplication.Core.Repositories;
+
+namespace NoteApplication.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext context;
+
+        public INoteRepository Note { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            this.context = context;
+            Note = new NoteRepository(context);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
